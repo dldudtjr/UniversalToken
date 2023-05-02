1c45b0a4ab1056a9a69fdf427fc39034d8150fb5
lys <dudtjr82@gmail.com>
2023-05-02 15:12:23 +0900

    token add

--- tasks/deployments/deploy-TestERC20.js
+++ tasks/deployments/deploy-TestERC20.js
@@ -2,13 +2,11 @@ module.exports = async function(callback) {
   try {
     const ERC20HoldableToken = artifacts.require("ERC20HoldableToken");
 
-    const from = "0x4EeABa74D7f51fe3202D7963EFf61D2e7e166cBa";
+    const from = "0x64397283445258612c4f265ab85b7b7c8321a53e";
 
-    const erc20 = await ERC20HoldableToken.new("Test Holdable ERC20", "TEST", 18, {
-      from: from,
-    });
+    const erc20 = await ERC20HoldableToken.new("Test Holdable ERC20", "TEST", 18, {from: "0x64397283445258612c4f265ab85b7b7c8321a53e"});
 
-    await erc20.mint(from, '1000000000000000000000000000');
+    await erc20.mint("0x64397283445258612c4f265ab85b7b7c8321a53e", '1000000000000000000000000000');
     
     console.log("ERC20HoldableToken deployed at: " + erc20.address);
 
